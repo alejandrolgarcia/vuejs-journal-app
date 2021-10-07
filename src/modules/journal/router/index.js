c5801c095fab0047fea72fa12e6a5564bfd4d703
Luis Garcia <lalejandro.1988@gmail.com>
2021-10-06 21:43:14 -0600

    Estructura Journal, ntryList component, Fab

---
+++ src/modules/journal/router/index.js
@@ -0,0 +1,13 @@
+
+export default {
+    name: 'journal',
+    component: () => import(/* webpackChunkName: "journal" */ '@/modules/journal/layouts/JournalLayout.vue'),
+    children: [
+        {
+            path: '',
+            name: 'no-entry',
+            component: () => import(/* webpackChunkName: "journal-no-entry" */ '@/modules/journal/views/NoEntrySelected.vue'),
+        }
+
+    ]
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <div class="btn btn-primary">Primary</div>
+    <div @click="goToJournal" class="btn btn-primary">Primary</div>
   </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      goToJournal() {
+        this.$router.push({ name: 'no-entry'})
+      }
+    },
+  }
+</script>
